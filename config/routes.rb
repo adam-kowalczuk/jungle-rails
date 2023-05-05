ff192be1da34705821bea02584a50e903793fb82
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-05-04 18:58:01 -0700

    Add 'Sessions' routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
 
   resources :users, only: %i[new create]
 
+  resources :sessions, only: %i[new create destroy]
+
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: %i[edit update show]
