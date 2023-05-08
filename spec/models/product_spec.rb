b7a71f49e30aa31f4ab00d9a25b77d6fe0e60aaa
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-05-08 16:44:40 -0700

    Create initial test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it 'should save successful when all four fields are filled out' do
+      @category = Category.new({ name: 'Evergreens' })
+      @product = Product.new({ name: 'Sweet Hops', price: 64.49, quantity: 40,
+                               category: @category })
+      expect(@product.save).to be true
+    end
+  end
 end
