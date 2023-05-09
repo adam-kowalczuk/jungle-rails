8d9714027cf3442e06cc22cd55d6ee6da3f060ae
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-05-09 16:54:08 -0700

    Add test to make sure all products show up on home page

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -7,4 +7,7 @@ describe("Home Page", () => {
   it("should show products on the page", () => {
     cy.get(".products article").should("be.visible");
   });
+  it("should show 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
 });
