e6540e08614b94dab15c44045e61d642c2d7c177
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-05-09 16:37:25 -0700

    Add test for showing products on page

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -4,4 +4,7 @@ describe("Home Page", () => {
   it("loads the home page", () => {
     cy.visit("/");
   });
+  it("should show products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
 });
