4f8932d6780c9b345a5a2e57a0cea6108e1ec7d3
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-05-09 17:01:32 -0700

    Disable rubocop warning

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
 
   def create
     # If the user is authenticated with credentials
-    if user = User.authenticate_with_credentials(params[:email], params[:password])
+    if user = User.authenticate_with_credentials(params[:email], params[:password]) # rubocop:disable Lint/AssignmentInCondition
       # Save the user id inside the browser cookie. This is how we keep the user
       # logged in when they navigate around our website.
       session[:user_id] = user.id
