65473cf5bf8860e6d5f1cf1c8575bf64254b5181
Adam Kowalczuk <adam.kowalczuk1@gmail.com>
2023-05-09 17:52:33 -0700

    Remove tests for confirming products show on page

--- cypress/integration/add_to_cart.spec.js
+++ cypress/integration/add_to_cart.spec.js
@@ -4,12 +4,6 @@ describe("Add to Cart", () => {
   it("loads the home page", () => {
     cy.visit("/");
   });
-  it("should show products on the page", () => {
-    cy.get(".products article").should("be.visible");
-  });
-  it("should show 2 products on the page", () => {
-    cy.get(".products article").should("have.length", 2);
-  });
   it("should add an item to the cart", () => {
     cy.get(":nth-child(1) > div > .button_to > .btn").click({ force: true });
     cy.get(".end-0 > .nav-link").should("have.length", 1);
